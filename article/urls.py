e734e22450fa97f9ec56102013ec6dfeaa8497f9
ykhosravii <khosraviy124@gmail.com>
2025-03-17 14:07:02 +0330

    added pagination and sorting to the article/data api

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import dataList
 
 urlpatterns = [
-    path('data', dataList.as_view(), name='data-list')
+    path('data/', dataList.as_view(), name='data-list')
 ]
--- article/views.py
+++ article/views.py
@@ -1,14 +1,21 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Article
+from rest_framework.pagination import PageNumberPagination
 from .serializers import ArticleSerializer
 
 
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10  # Adjust as needed
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 class dataList(APIView):
-    
+    pagination_class = CustomPagination
 
     def get(self, request, *args, **kwargs):
-        profiles = Article.objects.all().values()[:20]
-        serializer = ArticleSerializer(profiles, many=True)
-        return Response(serializer.data)
+        articles = Article.objects.all().order_by('created_at')  # Sorting by created_at descending
+        paginator = self.pagination_class()
+        result_page = paginator.paginate_queryset(articles, request)
+        serializer = ArticleSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
